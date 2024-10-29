86121307a0e6a305e783f27908dda8c7c2de9945
Hareesh T J <hareesh1996@gmail.com>
2024-10-29 14:10:03 +0530

    used build on port

--- backend/src/express.js
+++ backend/src/express.js
@@ -11,6 +11,7 @@ const expressConfig = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+    app.use(express.static('build'));
 
     app.use(
         cors({
