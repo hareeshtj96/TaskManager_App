b95a9493c38f86a8612dbf9fc669385476b6d31d
Hareesh T J <hareesh1996@gmail.com>
2024-10-29 12:12:01 +0530

    changed port

--- frontend/src/Services/userApi.js
+++ frontend/src/Services/userApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://taskmanager-app-test.onrender.com'
+const BASE_URL = 'http://localhost:9000'
 
 export const SIGNUPUSER = `${BASE_URL}/signup`;
 export const VERIFYOTP = `${BASE_URL}/verify_otp`;
